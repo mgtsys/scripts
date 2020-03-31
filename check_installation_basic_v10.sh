36be4e1e3a2aa1654534dfdd58ff08355ac79add
mgtsys <mgtsys@mgt-commerce.com>
2020-03-31 18:04:36 +0200

    Update check_installation_basic_v10.sh

--- check_installation_basic_v10.sh
+++ check_installation_basic_v10.sh
@@ -77,16 +77,41 @@ function check_ownership() {
             OWNER_FOLDER=$(ls -lrt "/${ROOT_DIR}" | awk 'NR == 2' | cut -d " " -f 4)
             if [[ "${OWNER_FOLDER}" != "${USERS[$i]}" ]]; then
                 MESS_PHP+="${NOT_OK}$File ${FILE} uses port ${NGINX_PORT} (user ${USERS[$i]}) and root folder's owner is ${OWNER_FOLDER}\n"
-                # echo -e "${NOT_OK}File ${FILE} uses port ${NGINX_PORT} (user ${USERS[$i]}) and root folder's owner is ${OWNER_FOLDER}"
             else
                 MESS_PHP+="${OK}$File ${FILE}\n"
                 chmod -R 775 "/${ROOT_DIR}/"
-                #MESS_PHP+="${OK}$Directory ${ROOT_DIR} 775 permissions have been set\n"
-                #resetting 775 permissions to make sure permissions are right. cannot check that all folders have 775. 
-                # echo "     Setting 775 permissions for root folder ${ROOT_DIR}" 
             fi
         fi
     done
+}
+
+# check if in magento 2 is pointing to pub folder 
+function check_pub_folder() {
+    # get the config magento file
+    PUB=$(sed -n "/root/p" ${FILE} | awk 'NR == 1' | cut -d "/" -f 6 | cut -d ";" -f 1)
+    CONFIG_FILE=$(ls -lrt "/${ROOT_DIR}/app/etc/" | sed -n "/local.xml/p;/env.php/p")
+    # we know that the file=env.php then is magento2 
+    if [[ "${CONFIG_FILE}" = *"env.php"* ]]; then
+        CONFIG_FILE="/${ROOT_DIR}/app/etc/env.php"
+        MAGENTO_VERSION=2
+        #IS MAGENTO 2 --> root directory should have pub and should be rwo media blocks in nginx. 
+        if [[ "${PUB}" != "pub" ]]; then 
+            MESS_NGINX+="${NOT_OK}${FILE} - Root folder does not contain /pub\n"
+        fi
+        # check if media blocks are in nginx config -> is a warning if there are not to check if images are loading
+        MEDIA_BLOCKS=$(sed -n "/media/p" ${FILE} | wc -l)
+        if [[ "${MEDIA_BLOCKS}" != "2" ]]; then 
+            MESS_NGINX+="     \e[38;5;198mWARNING: ${FILE} - You should have 'location /media/' block for 80 and 443 - check if images are loading\n"
+        fi
+    elif [[ "${CONFIG_FILE}" = *"local.xml"* ]]; then
+        # we set the config file (in case there are local.xml.bak etc,...)
+        CONFIG_FILE="/${ROOT_DIR}/app/etc/local.xml"
+        MAGENTO_VERSION=1
+        # should not have pub configued as in magento1 it does not exist
+        if [[ "${PUB}" == "pub" ]]; then 
+            MESS_NGINX+="${NOT_OK}${FILE} - Root folder contains /pub and should not because is Magento1\n"
+        fi
+    fi
 }
 
 # checks if the https redirection (from http -> https) is working. 
@@ -98,7 +123,6 @@ function check_https_redirection() {
         # we make sure we have not checked this server name before 
         if [[ $PREV_HOST != *"${SERVER_NAME}"* ]]; then
             # we get the response of curl with http
-            #REDIRECTION=$(curl -s -Ik http://"${SERVER_NAME}" --resolve ${SERVER_NAME}:${PRIVATE_IP})
             REDIRECTION=$(curl --resolve ${SERVER_NAME}:80:${PRIVATE_IP} http://${SERVER_NAME}:80 -Ik -s)
             # we get the server_name redirected (in case we are doing it with www and it redirects to without or with www1...)
             REDIRECTION_HTTPS=$(echo "${REDIRECTION,,}" | grep "location" | cut -d "/" -f 3)
@@ -115,122 +139,24 @@ function check_https_redirection() {
     done < <(sed -n "/server_name/p" ${FILE} | head -n 2)
 }
 
-
 function check_www_nonwww_redirection() {
-    if [[ ${REDIRECTION_HTTPS} = *"www"* ]]; then PLAIN_DOMAIN=$(echo ${REDIRECTION_HTTPS} | cut -d "." -f 2-)
-    else PLAIN_DOMAIN=${REDIRECTION_HTTPS}; fi 
-    # checking curl with non-www
-    CURL_NON_WWW=$(curl --resolve ${PLAIN_DOMAIN}:443:${PRIVATE_IP} https://${PLAIN_DOMAIN}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/location/p" | cut -d "/" -f 3)
-    CURL_WWW=$(curl --resolve www.${PLAIN_DOMAIN}:443:${PRIVATE_IP} https://www.${PLAIN_DOMAIN}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/location/p" | cut -d "/" -f 3)
-    #CURL_NON_WWW=$(curl -s -Ik https://"${PLAIN_DOMAIN}" --resolve ${PLAIN_DOMAIN}:${PRIVATE_IP} 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/location/p" | cut -d "/" -f 3)
-    #CURL_WWW=$(curl -s -Ik https://"www.${PLAIN_DOMAIN}" --resolve www.${PLAIN_DOMAIN}:${PRIVATE_IP} 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/location/p" | cut -d "/" -f 3)
-    # if there is not redirection to www-> nonwww or nonwww -> www then these two variables should not contain any information
-    if [[ ! -z ${CURL_NON_WWW} && ! -z ${CURL_WWW} ]]; then 
-        MESS_NGINX+="${NOT_OK}${PLAIN_DOMAIN} - You have not set a redirection for www <--> non-www\n"
-        # echo -e "${NOT_OK}You have not set a redirection for www <--> non-www"; 
-    fi 
-}
-
-function check_basic_auth_alias_test() {
-    BASIC_AUTH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/restricted/p" | cut -d '"' -f 2)
-    #BASIC_AUTH=$(curl -s -Ik https://"${REDIRECTION_HTTPS}" --resolve ${REDIRECTION_HTTPS}:${PRIVATE_IP} 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/restricted/p" | cut -d '"' -f 2)
-    if [[ "${BASIC_AUTH}" != *"restricted"* ]]; then
-        MESS_NGINX+="${NOT_OK}BASIC AUTH ${REDIRECTION_HTTPS} - Please enable Basic auth\n"
-        # we set this for when we are checking varnish 
-        IS_BASIC_AUTH=false
-    else 
-        MESS_NGINX+="${OK}BASIC AUTH ${REDIRECTION_HTTPS}\n"
-        IS_BASIC_AUTH=true; 
-    fi
-}
-
-function check_basic_auth_test() {
     IS_TEST=$(echo "${FILE}" | cut -d "." -f 1)
-    if [[ ${IS_TEST} == *"test"* || ${IS_TEST} == *"stage"* ]]; then
-        BASIC_AUTH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/restricted/p" | cut -d '"' -f 2)
-        #BASIC_AUTH=$(curl -s -Ik https://"${REDIRECTION_HTTPS}" --resolve ${REDIRECTION_HTTPS}:${PRIVATE_IP} 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/restricted/p" | cut -d '"' -f 2)
-        if [[ "${BASIC_AUTH}" != *"restricted"* ]]; then
-            MESS_NGINX+="${NOT_OK}BASIC AUTH ${REDIRECTION_HTTPS} - Please enable Basic auth\n"
-            # we set this for when we are checking varnish 
-            IS_BASIC_AUTH=false
-        else 
-            MESS_NGINX+="${OK}BASIC AUTH ${REDIRECTION_HTTPS}\n"
-            IS_BASIC_AUTH=true; 
+    if [[ ${IS_TEST} != *test* && ${IS_TEST} != *stage* ]]; then
+        if [[ ${REDIRECTION_HTTPS} = *"www"* ]]; then PLAIN_DOMAIN=$(echo ${REDIRECTION_HTTPS} | cut -d "." -f 2-)
+        else PLAIN_DOMAIN=${REDIRECTION_HTTPS}; fi 
+        # checking curl with non-www
+        CURL_NON_WWW=$(curl --resolve ${PLAIN_DOMAIN}:443:${PRIVATE_IP} https://${PLAIN_DOMAIN}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/location/p" | cut -d "/" -f 3)
+        CURL_WWW=$(curl --resolve www.${PLAIN_DOMAIN}:443:${PRIVATE_IP} https://www.${PLAIN_DOMAIN}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/location/p" | cut -d "/" -f 3)
+        # if there is not redirection to www-> nonwww or nonwww -> www then these two variables should not contain any information
+        if [[ -z ${CURL_NON_WWW} && -z ${CURL_WWW} ]]; then 
+            MESS_NGINX+="${NOT_OK}${PLAIN_DOMAIN} - You have not set a redirection for www <--> non-www\n"
+        elif [[ ! -z ${CURL_NON_WWW} ]]; then REDIRECTION_HTTPS=${CURL_NON_WWW}
+        else REDIRECTION_HTTPS=${CURL_WWW}
         fi
+    
     fi
 }
 
-# checking that the SSL certificate is up to date (valid)
-function check_ssl_ww1_test_cert() {
-    SSL_CERT=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} --insecure -v https://"${REDIRECTION_HTTPS}:443" --resolve ${REDIRECTION_HTTPS}:${PRIVATE_IP} 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | sed -n "/SSL certificate verify/p")
-    if [[ ${SSL_CERT} == *expired* ]]; then 
-        MESS_SSL+="${NOT_OK}${REDIRECTION_HTTPS} - SSL Certificate not ok -> Set Letsencrypt\n"
-      #  echo -e "${NOT_OK}SSL Certificate not ok -> install Letsencrypt"; 
-    elif [[ ${SSL_CERT} == *ok* ]]; then 
-        MESS_SSL+="${OK}${REDIRECTION_HTTPS}\n"
-    fi
-}
-
-# check if in magento 2 is pointing to pub folder 
-function check_pub_folder() {
-    # get the config magento file
-    PUB=$(sed -n "/root/p" ${FILE} | awk 'NR == 1' | cut -d "/" -f 6 | cut -d ";" -f 1)
-    CONFIG_FILE=$(ls -lrt "/${ROOT_DIR}/app/etc/" | sed -n "/local.xml/p;/env.php/p")
-    # we know that the file=env.php then is magento2 
-    if [[ "${CONFIG_FILE}" = *"env.php"* ]]; then
-        CONFIG_FILE="/${ROOT_DIR}/app/etc/env.php"
-        MAGENTO_VERSION=2
-        #IS MAGENTO 2 --> root directory should have pub and should be rwo media blocks in nginx. 
-        if [[ "${PUB}" != "pub" ]]; then 
-            MESS_NGINX+="${NOT_OK}${REDIRECTION_HTTPS} - Root folder does not contain /pub\n"
-            # echo -e "${NOT_OK}Root folder does not contain /pub"; 
-        fi
-        # check if media blocks are in nginx config -> is a warning if there are not to check if images are loading
-        MEDIA_BLOCKS=$(sed -n "/media/p" ${FILE} | wc -l)
-        if [[ "${MEDIA_BLOCKS}" != "2" ]]; then 
-            #echo -e "     \e[38;5;198mWARNING: \e[0mYou should have 'location /media/' block for 80 and 443 - check if images are loading"; 
-            MESS_NGINX+="     \e[38;5;198mWARNING: ${REDIRECTION_HTTPS} - You should have 'location /media/' block for 80 and 443 - check if images are loading\n"
-        fi
-    elif [[ "${CONFIG_FILE}" = *"local.xml"* ]]; then
-        # we set the config file (in case there are local.xml.bak etc,...)
-        CONFIG_FILE="/${ROOT_DIR}/app/etc/local.xml"
-        MAGENTO_VERSION=1
-        # should not have pub configued as in magento1 it does not exist
-        if [[ "${PUB}" == "pub" ]]; then 
-            MESS_NGINX+="${NOT_OK}${REDIRECTION_HTTPS} - Root folder contains /pub and should not because is Magento1\n"
-            # echo -e "${NOT_OK}Root folder contains /pub and should not because is Magento1"; 
-        fi
-    fi
-}
-
-function check_production_mode() {
-    MAGE_MODE=$(sed -n "/MAGE_MODE/p" ${CONFIG_FILE} | sed -n "/production/p")
-    # we get the mage mode (we will only have something if it is magento2 and there is prodution in the ouput)
-    if [[ "${MAGENTO_VERSION}" = 2 && -z "${MAGE_MODE}" ]]; then 
-        MESS_MAG_CONFIG+="${NOT_OK}${REDIRECTION_HTTPS} - Magento mode in ${CONFIG_FILE} is not set to Production\n"
-        #echo -e "${NOT_OK}Magento mode in ${CONFIG_FILE} is not set to Production"; 
-    fi
-    # we can also check it with bin/magento -> but we need more lines of code
-}
-
-# check if the admin path is the same in vhost and env.php/local.xml
-function check_admin_config() {
-    # in env.pphp file the format is --> 'frontName' => 'admin_xk9j7q'
-    # in local.xml the format is     --> <frontName><![CDATA[dashboard]]></frontName>
-    ADMIN_PATH=$(sed -n "/frontName/p" ${CONFIG_FILE} | cut -d ">" -f 2 | cut -d "'" -f 2)
-    # using cli
-    # ADMIN_PATH=$(/${ROOT_DIR}/bin/magento info:adminuri | sed -n '/URI/p' | cut -d "/" -f 2)
-    if [[ "${MAGENTO_VERSION}" = 1 ]]; then ADMIN_PATH=$(sed -n "/frontName/p" ${CONFIG_FILE} | cut -d "[" -f 3 | cut -d "]" -f 1); fi
-    if [[ ${ADMIN_PATH} ]]; then
-        # check if in vhost there is this admin path
-        IS_ADMIN=$(sed -n "/"${ADMIN_PATH}"/p" ${FILE} | sed -n "/location/p")
-        # if the admin paht is not in the vhost -> not ok 
-        if [[ -z ${IS_ADMIN} ]]; then 
-            MESS_NGINX+="${NOT_OK}${REDIRECTION_HTTPS} - In ${CONFIG_FILE} admin path is ${ADMIN_PATH} and in vhost is not this one\n"
-            # echo -e "${NOT_OK}In ${CONFIG_FILE} admin path is ${ADMIN_PATH} and in vhost is not this one"; 
-        fi
-    fi
-}
 
 function check_mysql_connection() {
     MYSQL_U=$(sed -n '/<connection>/,/<\/connection>/p' ${CONFIG_FILE} | sed -n "/username/p" | cut -d "[" -f 3 | cut -d "]" -f 1)
@@ -273,6 +199,72 @@ function check_secure_unsecure_url() {
             MESS_NGINX+="${NOT_OK}${BASE_URL} - You don't have set https in unsecure url in the database \n"
         fi
     done < <(mysql -s -h"localhost" -u"${MYSQL_U}" -p"${MYSQL_P}" ${MYSQL_DB} -e "SELECT value FROM core_config_data WHERE path LIKE '%/unsecure/base_url%'" | tail -n+1)
+}
+
+
+function check_basic_auth_alias_test() {
+    BASIC_AUTH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/restricted/p" | cut -d '"' -f 2)
+    if [[ "${BASIC_AUTH}" != *"restricted"* ]]; then
+        MESS_NGINX+="${NOT_OK}BASIC AUTH ${REDIRECTION_HTTPS} - Please enable Basic auth\n"
+        # we set this for when we are checking varnish 
+        IS_BASIC_AUTH=false
+    else 
+        MESS_NGINX+="${OK}BASIC AUTH ${REDIRECTION_HTTPS}\n"
+        IS_BASIC_AUTH=true; 
+    fi
+}
+
+function check_basic_auth_test() {
+    IS_TEST=$(echo "${FILE}" | cut -d "." -f 1)
+    if [[ ${IS_TEST} == *"test"* || ${IS_TEST} == *"stage"* ]]; then
+        BASIC_AUTH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/restricted/p" | cut -d '"' -f 2)
+        if [[ "${BASIC_AUTH}" != *"restricted"* ]]; then
+            MESS_NGINX+="${NOT_OK}BASIC AUTH ${REDIRECTION_HTTPS} - Please enable Basic auth\n"
+            # we set this for when we are checking varnish 
+            IS_BASIC_AUTH=false
+        else 
+            MESS_NGINX+="${OK}BASIC AUTH ${REDIRECTION_HTTPS}\n"
+            IS_BASIC_AUTH=true; 
+        fi
+    fi
+}
+
+# checking that the SSL certificate is up to date (valid)
+function check_ssl_ww1_test_cert() {
+    SSL_CERT=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} --insecure -v https://"${REDIRECTION_HTTPS}:443" --resolve ${REDIRECTION_HTTPS}:${PRIVATE_IP} 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | sed -n "/SSL certificate verify/p")
+    if [[ ${SSL_CERT} == *expired* ]]; then 
+        MESS_SSL+="${NOT_OK}${REDIRECTION_HTTPS} - SSL Certificate not ok -> Set Letsencrypt\n"
+    elif [[ ${SSL_CERT} == *ok* ]]; then 
+        MESS_SSL+="${OK}${REDIRECTION_HTTPS}\n"
+    fi
+}
+
+function check_production_mode() {
+    MAGE_MODE=$(sed -n "/MAGE_MODE/p" ${CONFIG_FILE} | sed -n "/production/p")
+    # we get the mage mode (we will only have something if it is magento2 and there is prodution in the ouput)
+    if [[ "${MAGENTO_VERSION}" = 2 && -z "${MAGE_MODE}" ]]; then 
+        MESS_MAG_CONFIG+="${NOT_OK}${REDIRECTION_HTTPS} - Magento mode in ${CONFIG_FILE} is not set to Production\n"
+    fi
+    # we can also check it with bin/magento -> but we need more lines of code
+}
+
+# check if the admin path is the same in vhost and env.php/local.xml
+function check_admin_config() {
+    # in env.pphp file the format is --> 'frontName' => 'admin_xk9j7q'
+    # in local.xml the format is     --> <frontName><![CDATA[dashboard]]></frontName>
+    ADMIN_PATH=$(sed -n "/frontName/p" ${CONFIG_FILE} | cut -d ">" -f 2 | cut -d "'" -f 2)
+    # using cli
+    # ADMIN_PATH=$(/${ROOT_DIR}/bin/magento info:adminuri | sed -n '/URI/p' | cut -d "/" -f 2)
+    if [[ "${MAGENTO_VERSION}" = 1 ]]; then ADMIN_PATH=$(sed -n "/frontName/p" ${CONFIG_FILE} | cut -d "[" -f 3 | cut -d "]" -f 1); fi
+    if [[ ${ADMIN_PATH} ]]; then
+        # check if in vhost there is this admin path
+        IS_ADMIN=$(sed -n "/"${ADMIN_PATH}"/p" ${FILE} | sed -n "/location/p")
+        # if the admin paht is not in the vhost -> not ok 
+        if [[ -z ${IS_ADMIN} ]]; then 
+            MESS_NGINX+="${NOT_OK}${REDIRECTION_HTTPS} - In ${CONFIG_FILE} admin path is ${ADMIN_PATH} and in vhost is not this one\n"
+            # echo -e "${NOT_OK}In ${CONFIG_FILE} admin path is ${ADMIN_PATH} and in vhost is not this one"; 
+        fi
+    fi
 }
 
 # as it is a new config we check that alias (www1) is right configured.
@@ -319,16 +311,17 @@ function check_live_domain() {
 }
 
 function check_admin_user() {
-    USER=$(mysql -s -h"localhost" -u"${MYSQL_U}" -p"${MYSQL_P}" ${MYSQL_DB} -e "SELECT username FROM admin_user WHERE username LIKE '%admin%'"| tail -n+1)
+    USER=$(mysql -s -h"localhost" -u"${MYSQL_U}" -p"${MYSQL_P}" ${MYSQL_DB} -e "SELECT username FROM admin_user WHERE username LIKE '%admin%'" | tail -n+1)
     if [[ -z ${USER} ]]; then
-        MESS_MAG_CONFIG+="${NOT_OK}${BASE_URL} - There isn't any "admin" user for the backend\n"
+        MESS_MAG_CONFIG+="${NOT_OK}${REDIRECTION_HTTPS} - There isn't any "admin" user for the backend\n"
+    else MESS_MAG_CONFIG+="${OK}${REDIRECTION_HTTPS} - User "admin" for backend\n"
     fi
 }
 
 function check_mgt_user() {
     USER=$(mysql -s -h"localhost" -u"${MYSQL_U}" -p"${MYSQL_P}" ${MYSQL_DB} -e "SELECT username FROM admin_user WHERE username LIKE '%mgt%'"| tail -n+1)
     if [[ -z ${USER} ]]; then
-        MESS_MAG_CONFIG+="${NOT_OK}${BASE_URL} - There isn't any "mgtcommerce" user for the backend\n"
+        MESS_MAG_CONFIG+="${NOT_OK}${REDIRECTION_HTTPS} - There isn't any "mgtcommerce" user for the backend\n"
     fi    
 }
 
@@ -350,14 +343,11 @@ function check_varnish() {
     fi
     sleep 1
     # we check if we received x-cache-age -> if varnish is working properly we should receive a number higher than 0 (the 2nd curl)
-    #VARNISH=$(curl -s -Ik https://${REDIRECTION_HTTPS} --resolve ${REDIRECTION_HTTPS}:${PRIVATE_IP} 2>&1)
     VARNISH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s)
-    #VARNISH=$(curl -s -Ik https://${REDIRECTION_HTTPS} --resolve ${REDIRECTION_HTTPS}:${PRIVATE_IP} 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/x-cache-age/p" | cut -d " " -f 2)
-    VARNISH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s 2>&1 | sed -e 's/\(.*\)/\L\1/' | sed -n "/x-cache-age/p" | cut -d " " -f 2)
-    #varnish=$(echo "${curl_https,,}" | grep "x-cache-age" | cut -d " " -f 2)
+    VARNISH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s)
+    VARNISH=$(curl --resolve ${REDIRECTION_HTTPS}:443:${PRIVATE_IP} https://${REDIRECTION_HTTPS}:443 -Ik -s | sed -e 's/\(.*\)/\L\1/' | sed -n "/x-cache-age/p" | cut -d " " -f 2)
     if [[ ${VARNISH} == "0"* || -z ${VARNISH} ]]; then 
         MESS_VARNISH+="${NOT_OK}${REDIRECTION_HTTPS} - Varnish is not working\n"; 
-        # echo -e "${NOT_OK}Varnish is not working"; 
     #    we should check thath the MGT extension of varnish is there --> if it is not working only.  
         if [[ ("${MAGENTO_VERSION}" = 2 && ! -d "/${ROOT_DIR}/app/code/Mgt/Varnish") || 
               ("${MAGENTO_VERSION}" = 1 && ! -d "/${ROOT_DIR}/app/code/community/Mgt/Varnish") ]]; then 
@@ -469,15 +459,15 @@ function check_cache_enabled() {
 function global() {
     check_ownership
     check_pub_folder
-    check_admin_config
-    check_mysql_connection
     check_https_redirection
-    check_secure_unsecure_url
     check_www_nonwww_redirection
+    check_admin_config
+    check_production_mode
+    check_mysql_connection
     check_secure_unsecure_url
     if [[ "${ENV}" = prod ]]; then 
         check_alias;
-        check_basic_auth; 
+        check_basic_auth_alias_test; 
         check_admin_user;
     else
         check_live_domain;
