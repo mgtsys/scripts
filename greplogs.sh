e90031b607168075e9c955731eefc9b8ea1105e9
mgtsys <mgtsys@mgt-commerce.com>
2021-10-29 23:54:37 +0530

    Update greplogs.sh
    Added Abuse IP Function

--- greplogs.sh
+++ greplogs.sh
@@ -153,6 +153,41 @@ fi
 
 }
 
+
+##Validate the Extracted Ips to the Abuse IPs Database
+Abuse_IPs() {
+
+##Strip HIT cout and IP address
+for k in ${IP[@]}; do
+   if [[ "${#k}" -le "2" ]];then
+IPHitCount=$k
+else
+AbuseIP=$k
+
+for i in ${AbuseIP[@]}; do
+
+##API Call To Fetch Json
+jsonout=$(curl -s -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=$i" -d maxAgeInDays=90 -d verbose  -H "Key: d8dc9983dc9b8364f5208f6b706dcbed4dda697e9a6cf922636a49c6b865054aaf70c75ee5147939" -H "Accept: application/json")
+
+echo -ne "\n*IP Address: $i"
+
+##Abuse Confidence Score
+AbuseConfidenceScore=$(echo $jsonout | awk -F [:] '{print $7}' | cut -d "," -f1)
+echo -ne "\n The Abuse Confidence Score is $AbuseConfidenceScore% \n"
+
+##Origin Country
+OriginCountry=$(echo $jsonout | awk -F [:] '{print $13}' | cut -d "," -f1)
+echo -ne " The IP origin country is $OriginCountry \n"
+
+##Domain Name
+DomainName=$(echo $jsonout | awk -F [:] '{print $11}' | cut -d "," -f1)
+echo -ne " The IP domain name is $DomainName \n"
+
+done
+   fi
+done
+}
+
 Options_Select() {
 
 echo -ne "\n Please choose appropirate options for the desired output: \n"
@@ -161,7 +196,8 @@ echo -ne "\n 1) Enter 1 to Extract Top 10 Unique IPs"
 echo -ne "\n 2) Enter 2 to Extract Top 10 User Agents"
 echo -ne "\n 3) Enter 3 to Extract Top 10 Bots"
 echo -ne "\n 4) Enter 4 to Extract Top 10 Urls"
-echo -ne "\n 5) Enter 5 to Extract All the Above\n"
+echo -ne "\n 5) Enter 5 to Extract All the Above"
+echo -ne "\n 6) Enter 6 to Extract Top 10 Unique IPs and verify them with Abuse IPs database\n"
 
 echo -ne "\n Enter Your Option: "
 read option
@@ -189,6 +225,11 @@ elif
     Extract_URLs
     exit
 elif
+   [ "$option" = 6 ];then
+   Extract_Unique_IPs
+   Abuse_IPs
+   exit
+elif
   ! [[ "$option" =~ "$int" ]] ; then
    echo -ne "\n Please Enter number from ( 1 - 5 ) to display correct output \n"
 exit
@@ -208,6 +249,7 @@ Options_Select
 #Extract_UA
 #Extract_Bots
 #Extract_URLs
+#Abuse_Ips
 }
 
 main
