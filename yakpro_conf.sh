2439fc091d964076b36fb60916415b2d27b50c32
Arvind Thevar <arvind.thevar@mgt-commerce.com>
2022-03-15 10:59:18 +0530

    Yakpro-po conf script

---
+++ yakpro_conf.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+INSTALL_DIR="/usr/local/yakpro-po"
+YAK_CONF="$INSTALL_DIR/yakpro-po.cnf"
+
+if [[ -n $(which yakpro-po) ]]
+then
+        if [[ -d $INSTALL_DIR ]]
+        then
+                sed -i 60s/true/false/ $YAK_CONF
+                sed -i 61s/true/false/ $YAK_CONF
+                sed -i 62s/true/false/ $YAK_CONF
+                sed -i 65s/true/false/ $YAK_CONF
+                sed -i 67s/true/false/ $YAK_CONF
+                sed -i 68s/true/false/ $YAK_CONF
+                while true;
+                do
+                        echo "Enter the Source path of your module/code(Path must be absolute): "
+                        read path
+                        if [[ -n $path ]];
+                        then
+                                sed -i 86s+"'.*'"+"'$path'"+ $YAK_CONF
+                        else
+                                continue
+                        fi
+                        echo "Enter the Output path for your obfuscated code(Path must be absolute): "
+                        read path2
+                        if [[ -n $path2 ]]
+                        then
+                                sed -i 87s+null+"'$path2'"+ $YAK_CONF
+                        else
+                                continue
+                        fi
+                        break
+                done
+                sed -i '105, 113 s/^/#/' /usr/local/yakpro-po/include/classes/config.php
+        fi
+else
+        git clone https://github.com/pk-fr/yakpro-po.git $INSTALL_DIR
+        cd $INSTALL_DIR
+        git clone https://github.com/nikic/PHP-Parser.git
+        chmod a+x yakpro-po.php
+        ln -s $INSTALL_DIR/yakpro-po.php /usr/local/bin/yakpro-po
+        echo -e "\e[0;32mRe-run the Script\e[0m"
+fi
